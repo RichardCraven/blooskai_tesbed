f1ac33d38eb5cf659a249cdd8b98009752af6a6b
Richard Craven <rcraven85@gmail.com>
2019-05-28 22:11:53 -0700

    cleanup

--- src/app/mid-panel/mid-panel.component.ts
+++ src/app/mid-panel/mid-panel.component.ts
@@ -15,9 +15,7 @@ export class MidPanelComponent implements OnInit {
   cropping: boolean = false;
   selectMode: boolean = false;
   imgObj: any;
-  loadTimer: any;
-  selectedFrameSrc: any;
-  paneContainer: any;
+  loadTimer: any; 
   thumbnailContainer: any;
 
   rect = {
@@ -32,53 +30,39 @@ export class MidPanelComponent implements OnInit {
   constructor(private dataTransfer: DataTransferService) { }
 
   ngOnInit() {
-    console.log('mid panel init');
     this.boundingCanvasInit();
-    this.paneContainer = document.getElementsByClassName('preview-pane-container')[0];
+    
     this.thumbnailContainer = document.getElementsByClassName('captured-thumbnail')[0];
 
-    const paneImage = new Image();
-    paneImage.height = 625;
-    paneImage.src = '../../../assets/sample.png'
-    this.paneContainer.appendChild(paneImage);
-
     this.subscription = this.dataTransfer.getData().subscribe(res => {
+      if(res.destination !== 'mid') return;
+      console.log('mid receiving data with type ', typeof res.data);
       res.destination === 'mid' && console.log('receiving data, data is ', res.data)
+      if(typeof res.data === 'string'){
+        this.selectMode = !this.selectMode;
+      }
     })
   }
   boundingCanvasInit(){
     this.boundingCanvas =  document.getElementById('bounding-canvas') as HTMLCanvasElement;
     this.ctx = this.boundingCanvas.getContext('2d');
-    // this.boundingCanvas.dr
   }
   mouseMove(e) {
-    var rect = this.boundingCanvas.getBoundingClientRect();
     if (this.drag) {
+      var rect = this.boundingCanvas.getBoundingClientRect();
       this.rect.w = (e.clientX - rect.left) - this.rect.startX;
       this.rect.h = (e.clientY - rect.top) - this.rect.startY ;
       this.ctx.clearRect(0,0,this.boundingCanvas.width,this.boundingCanvas.height);
-      // console.log(this.rect.w)
       this.draw();
     }
   }
   mouseDown(e) {
     var rect = this.boundingCanvas.getBoundingClientRect();
-
     this.rect.startX = e.clientX - rect.left;
     this.rect.startY = e.clientY - rect.top;
-
-    console.log(this.rect.startX)
-    console.log(this.rect.startY)
     this.drag = true;
     this.cropping = true;
   }
-  // getMousePos(canvas, evt) {
-  //   var rect = canvas.getBoundingClientRect();
-  //   return {
-  //     x: evt.clientX - rect.left,
-  //     y: evt.clientY - rect.top
-  //   };
-  // }
   mouseUp(e) {
     this.drag = false;
     if(this.cropping){
@@ -87,8 +71,8 @@ export class MidPanelComponent implements OnInit {
       this.rect.endY = e.clientY - rect.top;
       this.readyToCrop = true;
       this.selectMode = false;
-      console.log('rect: ', this.rect);
       this.dataTransfer.passData('top', this.rect)
+      this.selectMode = false;
     }
   }
   draw(){
@@ -96,92 +80,4 @@ export class MidPanelComponent implements OnInit {
     this.ctx.lineWidth = 4;
     this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h);
   }
-  
-  extractFrames(source) {
-    console.log('THIS IS ', this, 'source is ', source.target)
-    const that = this;
-    const self = source.target;
-    var video = document.createElement('video');
-    video.src = URL.createObjectURL(self.files[0]);
-    var array = [];
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    var pro = document.querySelector('#progress');
-
-    function initCanvas(e) {
-      canvas.width = this.videoWidth;
-      canvas.height = this.videoHeight;
-    }
-
-    function drawFrame(e) {
-      this.pause();
-      ctx.drawImage(this, 0, 0);
-      /* 
-      this will save as a Blob, less memory consumptive than toDataURL
-      a polyfill can be found at
-      https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
-      */
-      canvas.toBlob(saveFrame, 'image/jpeg');
-      pro.innerHTML = ((this.currentTime / this.duration) * 100).toFixed(2) + ' %';
-      if (this.currentTime < this.duration) {
-        this.play();
-      }
-    }
-
-    function saveFrame(blob) {
-      array.push(blob);
-    }
-
-    function revokeURL(e) {
-      // console.log('in revoke, e is ', e)
-      URL.revokeObjectURL(this.src);
-    }
-    
-    function onend(e) {
-      var img;
-      // do whatever with the frames
-      const container = document.getElementsByClassName('frame-container')[0]
-      // const paneContainer = document.getElementsByClassName('preview-pane-container')[0];
-      for (var i = 0; i < array.length; i++) {
-        img = new Image();
-        img.width = container.clientWidth/array.length*2
-        img.height = 200
-        img.onload = revokeURL;
-        img.src = URL.createObjectURL(array[i]);
-        container.appendChild(img);
-        img.addEventListener('mouseover', function(){
-          if(!that.selectMode) return;
-          that.paneContainer.innerHTML = '';
-          ctx.drawImage(this, 0, 0);
-          canvas.toBlob(saveFrame, 'image/jpeg');
-
-          let paneImage = new Image();
-          paneImage.width = that.paneContainer.clientWidth
-          paneImage.height = that.paneContainer.clientHeight
-          paneImage.onload = revokeURL;
-          paneImage.src = URL.createObjectURL(array[array.length-1]);
-          that.selectedFrameSrc = URL.createObjectURL(array[array.length-1])
-          that.paneContainer.appendChild(paneImage);
-          array.shift()
-        })
-        img.addEventListener('mousedown', function(){
-          if(!that.selectMode) return;
-          this.setAttribute("style", "border: 2px solid blue")
-        })
-      }
-      // we don't need the video's objectURL anymore
-      URL.revokeObjectURL(self.src);
-
-      array = [];
-    }
-    
-    video.muted = true;
-
-    video.addEventListener('loadedmetadata', initCanvas, false);
-    video.addEventListener('timeupdate', drawFrame, false);
-    video.addEventListener('ended', onend, false);
-
-    video.play();
-  }
-
 }
--- src/app/top-panel/top-panel.component.ts
+++ src/app/top-panel/top-panel.component.ts
@@ -12,6 +12,9 @@ export class TopPanelComponent implements OnInit {
   subscription: Subscription;
   copyCanvas: HTMLCanvasElement;
   copyCtx: any;
+  selectedFrameSrc: any;
+  paneContainer: any;
+  selectMode: boolean = false;
   rect = {
     startX : undefined,
     startY : undefined,
@@ -21,32 +24,33 @@ export class TopPanelComponent implements OnInit {
     w : 0
   };
   constructor(private dataTransfer: DataTransferService) { }
-
+// the grass isnt always greener, the brown spots are just in different places
   ngOnInit() {
-    console.log(this.dataTransfer)
+    this.paneContainer = document.getElementsByClassName('preview-pane-container')[0];
     this.subscription = this.dataTransfer.getData().subscribe(res => {
       if(res.destination !== 'top') return;
-      this.rect = res.data
-      console.log('received rect', this.rect)
+      if(typeof res.data !== 'string'){
+        this.rect = res.data;
+        this.selectMode = false;
+      }
     })
     this.copyCanvas = <HTMLCanvasElement> document.getElementById('copy-canvas');
     this.copyCtx = this.copyCanvas.getContext('2d');
   }
-  initiateCrop(src){
+  selectModeToggle(){
+    this.selectMode = !this.selectMode;
+    if(this.selectMode) this.dataTransfer.passData('mid', 'select')
+  }
+  initiateCrop(){
     console.log('initiating crop');
-    // console.log('kkk',this.selectedFrameSrc, this.selectMode, this.readyToCrop)
-    // if(this.selectMode || !this.readyToCrop ) return
     this.imgObj = new Image(625, 625);
-    this.imgObj.src = '../../../assets/sample.png'
+    this.imgObj.src = this.selectedFrameSrc
     const that = this;
     this.imgObj.onload = function(){
-      that.onPreloadComplete()
+      that.crop()
     };
   }
-  onPreloadComplete(){
-    // this.copyCtx.drawImage(this.imgObj, 0, 0, 625, 625)
-    // return
-    
+  crop(){
     const thumbnail = new Image(625, 625);
 
     const canvas = <HTMLCanvasElement> document.getElementById('thumbnail-canvas')
@@ -54,14 +58,9 @@ export class TopPanelComponent implements OnInit {
 
     var newImgSrc = this.getImagePortion(this.imgObj, this.rect.w, this.rect.h, this.rect.startX, this.rect.startY, 1);
     thumbnail.src = newImgSrc;
-    console.log('thumbnail is ', thumbnail)
-    ctx.drawImage(thumbnail, 0, 0)
-    // document.getElementsByClassName('top-window')[0].appendChild(thumbnail)
-    // this.copyCtx.drawImage(thumbnail, 0, 0, 625, 625)
+    ctx.drawImage(thumbnail, 0, 0);
   }
   getImagePortion(imgObj, newWidth, newHeight, startX, startY, ratio){
-    console.log('new w and h : ', newWidth, newHeight)
-    /* the parameters: - the image element - the new width - the new height - the x point we start taking pixels - the y point we start taking pixels - the ratio */
     //set up canvas for thumbnail
     var tnCanvas = document.createElement('canvas');
     var tnCanvasContext = tnCanvas.getContext('2d');
@@ -80,11 +79,91 @@ export class TopPanelComponent implements OnInit {
     const canvas = <HTMLCanvasElement> document.getElementById('thumbnail-canvas')
     const ctx = canvas.getContext('2d');
     ctx.drawImage(bufferCanvas, startX,startY,newWidth, newHeight, 0, 0, 300, 300);
-    /* now we use the drawImage method to take the pixels from our bufferCanvas and draw them into our thumbnail canvas */
-    // this.copyCtx.drawImage(bufferCanvas, startX,startY,newWidth, newHeight, startX, startY, newWidth, newHeight);
     this.copyCtx.drawImage(bufferCanvas, startX,startY,newWidth, newHeight, startX, startY, newWidth, newHeight);
     tnCanvasContext.drawImage(bufferCanvas, startX,startY,newWidth, newHeight, startX, startY, newWidth, newHeight);
 
     return tnCanvas.toDataURL();
-   }
+  }
+  extractFrames(source) {
+    const that = this;
+    const self = source.target;
+    var video = document.createElement('video');
+    video.src = URL.createObjectURL(self.files[0]);
+    var array = [];
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var pro = document.querySelector('#progress');
+
+    function initCanvas(e) {
+      canvas.width = this.videoWidth;
+      canvas.height = this.videoHeight;
+    }
+
+    function drawFrame(e) {
+      this.pause();
+      ctx.drawImage(this, 0, 0);
+      /* 
+      this will save as a Blob, less memory consumptive than toDataURL
+      a polyfill can be found at
+      https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
+      */
+      canvas.toBlob(saveFrame, 'image/jpeg');
+      pro.innerHTML = ((this.currentTime / this.duration) * 100).toFixed(2) + ' %';
+      if (this.currentTime < this.duration) {
+        this.play();
+      }
+    }
+
+    function saveFrame(blob) {
+      array.push(blob);
+    }
+
+    function revokeURL(e) {
+      // console.log('in revoke, e is ', e)
+      URL.revokeObjectURL(this.src);
+    }
+    
+    function onend(e) {
+      const container = document.getElementsByClassName('frame-container')[0];
+      for (var i = 0; i < array.length; i++) {
+        let img = new Image();
+        img.width = container.clientWidth/array.length*2
+        img.height = 200
+        img.onload = revokeURL;
+        img.src = URL.createObjectURL(array[i]);
+        container.appendChild(img);
+        img.addEventListener('mouseover', function(){
+          if(!that.selectMode) return;
+          that.paneContainer.innerHTML = '';
+          ctx.drawImage(this, 0, 0);
+          canvas.toBlob(saveFrame, 'image/jpeg');
+
+          let paneImage = new Image();
+          paneImage.width = that.paneContainer.clientWidth
+          paneImage.height = that.paneContainer.clientHeight
+          paneImage.onload = revokeURL;
+          
+          const frame = array[array.length-1]
+          if(frame !== undefined){
+            paneImage.src = URL.createObjectURL && URL.createObjectURL(frame);
+            that.selectedFrameSrc = URL.createObjectURL(array[array.length-1])
+            that.paneContainer.appendChild(paneImage);
+            array.shift()
+          }
+        })
+        img.addEventListener('mousedown', function(){
+          if(!that.selectMode) return;
+          this.setAttribute("style", "border: 2px solid blue")
+        })
+      }
+      // we don't need the video's objectURL anymore
+      URL.revokeObjectURL(self.src);
+      array = [];
+    };
+    video.muted = true;
+    video.addEventListener('loadedmetadata', initCanvas, false);
+    video.addEventListener('timeupdate', drawFrame, false);
+    video.addEventListener('ended', onend, false);
+    video.play();
+  }
 }
